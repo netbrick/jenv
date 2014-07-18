bf010f9705725e0f60dfe943afdeb530254e76c4
David Herman <david.herman@netbrick.cz>
2014-07-18 11:20:47 +0200

    Change completion names

--- commands/completion.sh
+++ commands/completion.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ "${JENV_SHELL}" == "zsh" ]]; then
-   source "${JENV_DIR}/commands/zsh-completion.sh"
+   source "${JENV_DIR}/commands/completion-zsh.sh"
 else
-   source "${JENV_DIR}/commands/bash-completion.sh"
+   source "${JENV_DIR}/commands/completion-bash.sh"
 fi
